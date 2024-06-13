8b4000ccd8388916fc0f93b96a94d01c24af2139
MissaelCV <missaelcv0220@gmail.com>
2024-06-12 20:31:14 -0400

    Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component, ComponentRef } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './page/products/products.component';
 
 export const routes: Routes = [];
+{
+    
+}
+
+
---
+++ src/app/page/car/car.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  standalone: true,
+  imports: [],
+  templateUrl: './car.component.html',
+  styleUrl: './car.component.css'
+})
+export class CarComponent {
+
+}
