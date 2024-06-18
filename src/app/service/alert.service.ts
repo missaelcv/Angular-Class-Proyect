1a1a2cc19b77470f2a6a59fd8aad50f17ad8096a
missaelcv <58403931+missaelcv@users.noreply.github.com>
2024-06-18 18:55:40 -0400

    import sweetalert 2
    import sweetalert 2

--- src/app/service/alert.service.ts
+++ src/app/service/alert.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import Swal from 'sweetalert2'
 //Decorador 
 
 @Injectable ({
@@ -7,6 +8,18 @@ import { Injectable } from "@angular/core";
 })
 
 export class AlertService {
+    questionAlert(
+        title: string,
+        text: string,
+        confirmButtoText: string,
+        cancelButtonText: string
+    ) {
+        return Swal.fire({
+            title: title,
+            text: text,
+            backdrop: 'rgba(255,255,255)',
+        })
+    }
 
 }
 
