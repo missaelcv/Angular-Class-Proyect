0573122991ce38dae43b21d309e382481bac6f7d
MissaelCV <missaelcv0220@gmail.com>
2024-06-17 22:54:46 -0400

    importation Componet

--- src/app/page/products/products.component.ts
+++ src/app/page/products/products.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [
+   RouterModule,
+   HeaderComponent, 
+   FooterComponent,
+   RouterOutlet
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
