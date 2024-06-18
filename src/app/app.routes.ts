c12951c2cbfee13713b42023833fd6e56789899d
missaelcv <58403931+missaelcv@users.noreply.github.com>
2024-06-18 18:32:19 -0400

    Home Component
    Lead fs-4
    Btn Dark
    Lorem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { ApplicationModule, Component, ComponentRef } from '@angular/core';
+
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './page/products/products.component';
 import { CarComponent } from './page/car/car.component';
