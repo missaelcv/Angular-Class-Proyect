d014d8e388934a2f55b70ff725c8872682de6b83
MissaelCV <missaelcv0220@gmail.com>
2024-06-17 22:56:12 -0400

    Ajusten Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,26 @@
-import { Component, ComponentRef } from '@angular/core';
+import { ApplicationModule, Component, ComponentRef } from '@angular/core';
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './page/products/products.component';
+import { Action } from 'rxjs/internal/scheduler/Action';
+import { CarComponent } from './page/car/car.component';
 
-export const routes: Routes = [];
-{
-    
-}
+export const routes: Routes = [
+   {
+    path: 'products ',
+    component: ProductsComponent
+   },
+
+   {
+    path: 'car',
+    component: CarComponent
+   },
+
+   {
+    path: '',
+    redirectTo: 'HomeComponet',
+    pathMatch: 'full'
+   }
+
+];
 
 
