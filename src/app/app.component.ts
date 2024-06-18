2db61d8f746488bcf9c506181e96592ab4bfd4b1
missaelcv <58403931+missaelcv@users.noreply.github.com>
2024-06-18 10:48:17 -0400

    update app.componet.ts
    New Componet RouterModule y Outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FooterComponent, HeaderComponent]
+    imports: [
+        CommonModule,
+        RouterModule,
+        RouterOutlet,
+        FooterComponent,
+        HeaderComponent]
 })
 export class AppComponent {
   title = 'my-app';
